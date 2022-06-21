0a4859bf4e32fa2ebc82473fb89a2c140f6c7b80
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-06-21 20:48:52 -0300

    criada a estrutura visual do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import {useEffect} from "react";
 import {gql, useQuery } from "@apollo/client";
 import {client} from "./lib/apollo";
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
-  interface Lesson{
-    id: string;
-    title: string;
-  }
-
   return (
-     <ul>
-      {data?.lessons.map(lesson=>{
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-     </ul>
+    <Event/>
   )
 }
 
